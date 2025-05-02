d50bf0ebff77af1e00b14fb64b497ddbb3bfe263
AhmedAbdelgawadHassan <ahmedabdelgawad.developer@gmail.com>
2025-05-02 21:17:58 +0300

    Create MediTrack project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const MediTrack());
+}
+
+
+class MediTrack extends StatelessWidget {
+  const MediTrack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
